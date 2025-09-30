feb6a17658ebcffd36255f34e98ea789adfc4c4a
Ephemera <kwanghaualex24s@forward.edu.my>
2025-09-30 10:15:09 +0800

    Added a TypeConverter for the List Category

--- app/src/main/java/com/fwrdgrp/recipesaving/database/MyDatabase.kt
+++ app/src/main/java/com/fwrdgrp/recipesaving/database/MyDatabase.kt
@@ -2,13 +2,16 @@ package com.fwrdgrp.recipesaving.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.fwrdgrp.recipesaving.data.models.recipe.Ingredient
 import com.fwrdgrp.recipesaving.data.models.recipe.Instruction
 import com.fwrdgrp.recipesaving.data.models.recipe.Recipe
 import com.fwrdgrp.recipesaving.data.models.recipe.RecipeIngredient
+import com.fwrdgrp.recipesaving.data.utils.Converters
 
 
 @Database(entities = [Ingredient::class, Recipe::class, RecipeIngredient::class, Instruction::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class MyDatabase: RoomDatabase() {
     abstract fun getRecipeDao(): RecipeDao
 
